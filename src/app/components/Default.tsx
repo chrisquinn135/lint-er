a97ee7901a4b4e7fe411b7258f659f56a43635da
chrisquinn135 <85804252+chrisquinn135@users.noreply.github.com>
2023-03-01 15:40:54 -0800

    message update

--- src/app/components/Default.tsx
+++ src/app/components/Default.tsx
@@ -6,14 +6,10 @@ const Default = () => {
     <div className="spacing-horizontal-16 body flexbox">
         <img src={info} style={{width:'40%'}} />
         <div className="color-text--state-primary text-xl-semibold">TDS Linter</div>
-        <ol >
-          <li className="color-text--state-secondary">
-            What is a linter? It is used to calculate the amount of errors u have in ur design file! 
-          </li>
-          <li className="color-text--state-secondary">
-            Correct the errors and they disappear yay!
-          </li>
-        </ol>
+        <div className="color-text--state-secondary " style={{textAlign:'center', padding:'8px'}}>
+          Once you press "Run Lint", the linter will analyze your files using predefined rules from the Trulioo design system and display any design issues on the screen!     
+
+        </div>
         
     </div>
   )
